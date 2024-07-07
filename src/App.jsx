ddb04ff872a168c7c666ea7ec95715e67ee19058
ritchie-gr8 <rit.yotsathon@gmail.com>
2024-07-07 18:19:53 +0700

    feat: update ans

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,60 @@
+import { useCallback, useState } from "react";
 import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const url = `https://www.googleapis.com/books/v1/volumes?q=`;
+  // const [bookName, setBookName] = useState("");
+  const [booksList, setBooksList] = useState([]);
+
+  const debounce = (func) => {
+    let timer;
+    return function (...args) {
+      const context = this;
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(() => {
+        timer = null;
+        func.apply(context, args);
+      }, 500);
+    };
+  };
+
+  const fetchingBooks = async (query) => {
+    if (!query) return;
+
+    try {
+      const result = await axios.get(url + query);
+      setBooksList(result.data.items);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // useEffect(() => {
+  //   fetchingBooks();
+  // }, [bookName]);
+
+  const debouncedFetch = useCallback(debounce(fetchingBooks), [])
+
+  const handleInputChange = e => {
+    const value = e.target.value
+    debouncedFetch(value)
+  }
+
+  return (
+    <div className="App">
+      <h1>Find a Book</h1>
+      <input type="text" 
+      onChange={(e) => (handleInputChange(e))
+      } />
+      <ul>
+        {booksList.map((book) => (
+          <li key={book.id}>{book.volumeInfo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
